dd5bfa59d38329d11cd25e0758b9157696d65d8d
Jacob Tutor <tutorjacob@hotmail.com>
2023-01-18 05:09:35 -0600

    health display for player
    added a working text health display showing players current and max health (HEALTH: 27/50 for example)

--- GameJam/Assets/Scripts/TextHealth.cs
+++ GameJam/Assets/Scripts/TextHealth.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHealth : MonoBehaviour
+public class TextHealth : MonoBehaviour
 {
-    public int maxHealth = 50;
     public int currentHealth;
-    public bool vulnerable;
+    public int maxHealth;
     public Text healthText;
 
     // Start is called before the first frame update
@@ -16,18 +15,9 @@ public class PlayerHealth : MonoBehaviour
         currentHealth = maxHealth;
     }
 
+    // Update is called once per frame
     void Update()
     {
         healthText.text = currentHealth.ToString() + " / " + maxHealth.ToString();
-    }
-
-    public void TakeDmg(int amount)
-    {
-        currentHealth -= amount;
-
-        if (currentHealth <= 0)
-        {
-            Destroy(gameObject);
-        }
     }
 }
