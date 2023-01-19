05ca6ef56bd79e3750507ad51ffb404d231a42d4
Jacob Tutor <tutorjacob@hotmail.com>
2023-01-19 03:54:52 -0600

    New scenes and store
    base templates for the main menu and stores have been added

---
+++ GameJam/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadStore1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadLvl1()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
